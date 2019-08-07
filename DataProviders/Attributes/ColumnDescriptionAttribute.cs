9ff165fbb0c70b30e0a67ef771037717926a15e9
Khan <wokhan@outlook.com>
2019-08-07 21:05:03 +0200

    Generate ColumnDescription list from an actual type.

---
+++ DataProviders/Attributes/ColumnDescriptionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wokhan.Data.Providers.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ColumnDescriptionAttribute : Attribute
+    {
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+    }
+}
--- DataProviders/ColumnDescription.cs
+++ DataProviders/ColumnDescription.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Wokhan.Data.Providers.Attributes;
 
 namespace Wokhan.Data.Providers.Bases
 {
@@ -9,5 +13,23 @@ namespace Wokhan.Data.Providers.Bases
         public string Description { get; set; }
         public Type Type { get; set; }
         public string Category { get; set; }
+
+        public static List<ColumnDescription> FromType(Type sourceType)
+        {
+            return sourceType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .Select(p =>
+            {
+                var details = p.GetCustomAttributes<ColumnDescriptionAttribute>(true).FirstOrDefault();
+                return new ColumnDescription
+                {
+                    Name = p.Name,
+                    DisplayName = details?.DisplayName ?? p.Name,
+                    Description = details?.Description ?? p.Name,
+                    Type = p.PropertyType,
+                    Category = details?.Category ?? "Default"
+                };
+            })
+            .ToList();
+        }
     }
 }
